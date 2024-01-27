7f81666ffe380c03930dcbc8eb4d6663661a8c51
Sam D <samvelxdavtyan@gmail.com>
2024-01-27 12:23:15 +0400

    wip: add inflation adjustment range slider to demo

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
-import cuppy from "@cuppy/core/src"; // TODO: import from @cuppy/core
-import cpiU from "@cuppy/cpi-u/index"; // TODO: import from @cuppy/cpi-u
+import cuppy from "@cuppy/core";
+import cpiU from "@cuppy/cpi-u";
 
 cuppy.options.hint = "to";
 cuppy.options.currencyStyle = "code";
@@ -9,5 +9,41 @@ cuppy.options.useGrouping = false;
 cuppy.options.maxDigits = 3;
 
 cuppy.dataset(cpiU);
+
+const years = cuppy.datasets["cpiU"]!.years;
+const minYear = years[0].toString();
+const maxYear = new Date().getFullYear().toString();
+
+const input = document.getElementById("to-year");
+const minLabel = document.getElementById("min-label");
+const maxLabel = document.getElementById("max-label");
+
+input?.setAttribute("min", minYear);
+minLabel?.append(minYear);
+
+input?.setAttribute("max", maxYear);
+maxLabel?.append(maxYear);
+
+input?.setAttribute("value", maxYear);
+
+const yearValues = document.getElementById("year-values");
+yearValues?.append(
+  ...years.map((year) => {
+    const option = document.createElement("option");
+    option.value = year.toString();
+    option.append(year.toString());
+
+    return option;
+  }),
+);
+
+input?.addEventListener(
+  "input",
+  function (this: HTMLInputElement) {
+    cuppy.options.to = +this.value;
+    cuppy();
+  },
+  false,
+);
 
 cuppy();
